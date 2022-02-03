c66fa06c7af53f68e0af445df468a97be5e22582
AnonymeUSB <Allyssisation@gmail.com>
2022-02-03 15:01:45 +0000

    8

---
+++ script.js
@@ -0,0 +1,11 @@
+var droite = document.getElementById("fleche_droite");
+var gauche = document.getElementById("fleche_gauche");
+var diapo1 = document.getElementById("diapo1");
+
+function fleche_gauche(){
+    diapo1.style.display = "none";
+}
+
+function fleche_droite (){
+    diapo1.style.display = "none";
+}
