bb9d67d889b1458502475cbadcb4abe685703daf
AnonymeUSB <Allyssisation@gmail.com>
2022-02-03 15:09:26 +0000

    9

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var droite = document.getElementById("fleche_droite");
 var gauche = document.getElementById("fleche_gauche");
 var diapo1 = document.getElementById("diapo1");
+var count = 0;
 
 function fleche_gauche(){
-    diapo1.style.display = "none";
+
+    if (count === 0) {
+        alert("Pas de page avant.");
+    }
+    else {
+        diapo1.style.display = "none";
+        var count = count - 1;
+    }
+
 }
+
 
 function fleche_droite (){
     diapo1.style.display = "none";
+    var count = count + 1;
 }
