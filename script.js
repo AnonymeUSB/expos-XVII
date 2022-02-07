1a477de779c0ef1ccbacc05afd50f001f4592c7c
AnonymeUSB <Allyssisation@gmail.com>
2022-02-07 06:51:26 +0000

    14

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ sessionStorage.setItem('diapo', 1);
 
 function fleche_gauche(){
 
-    var diapo = sessionStorage.getItem('diapo') - 1;
+    var diapo = sessionStorage.getItem('diapo');
+    var diapo = diapo - 1;
 
     if (diapo < 1) {
         alert("Pas de page avant.");
+        var diapo = 1;
     }
     else {
-
-        alert(diapo);
         diapo1.style.display = "none";
     }
-
+    
+    if (diapo1 === 1){
+        diapo1.style.display = "block";
+    }
+    sessionStorage.setItem('diapo', diapo);
 }
 
 
@@ -25,10 +29,14 @@ function fleche_droite (){
     var diapo = parseInt(sessionStorage.getItem('diapo'));
     var diapo = diapo + 1;
 
+
     if (diapo > 2) {
-        alert("Pas de page après. " + typeof(diapo) + diapo);
+        alert("Pas de page après. ");
+        var diapo = 2;
     }
     else {
         diapo1.style.display = "none";
     }
+
+    sessionStorage.setItem('diapo', diapo);
 }
