e50201ac23f20caba9c0be271d54a68928d9d71e
AnonymeUSB <Allyssisation@gmail.com>
2022-02-23 07:24:31 +0000

    44

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var droite = document.getElementById("fleche_droite");
 var gauche = document.getElementById("fleche_gauche");
 var diapo1 = document.getElementById("diapo1");
 var diapo5 = document.getElementById("diapo5");
+var diapo6 = document.getElementById("diapo6");
 diapo5.style.diplay = "none";
 
 sessionStorage.setItem('diapo', 1);
@@ -19,6 +20,7 @@ function fleche_gauche(){
     else {
         if (diapo === 1){
             diapo1.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo2.style.display = "none";
@@ -26,6 +28,7 @@ function fleche_gauche(){
         }
         if (diapo === 2) {
             diapo2.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo1.style.display = "none";
@@ -33,12 +36,14 @@ function fleche_gauche(){
         }
         if (diapo === 3) {
             diapo3.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo2.style.display = "none";
             diapo1.style.display = "none";
         }
         if (diapo === 4) {
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "block";
             diapo3.style.display = "none";
@@ -46,6 +51,7 @@ function fleche_gauche(){
             diapo1.style.display = "none";
         }
         if (diapo === 5) {
+            diapo6.style.display = "none";
             diapo5.style.display = "block";
             diapo4.style.display = "none";
             diapo3.style.display = "none";
@@ -67,13 +73,14 @@ function fleche_droite (){
     var diapo = diapo + 1;
 
 
-    if (diapo > 5) {
+    if (diapo > 6) {
         alert("Pas de page après. ");
-        var diapo = 5;
+        var diapo = 6;
     }
     else {
         if (diapo === 1){
             diapo1.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo2.style.display = "none";
@@ -81,6 +88,7 @@ function fleche_droite (){
         }
         if (diapo === 2) {
             diapo2.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo1.style.display = "none";
@@ -88,12 +96,14 @@ function fleche_droite (){
         }
         if (diapo === 3) {
             diapo3.style.display = "block";
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo2.style.display = "none";
             diapo1.style.display = "none";
         }
         if (diapo === 4) {
+            diapo6.style.display = "none";
             diapo5.style.display = "none";
             diapo4.style.display = "block";
             diapo3.style.display = "none";
@@ -101,7 +111,16 @@ function fleche_droite (){
             diapo1.style.display = "none";
         }
         if (diapo === 5) {
+            diapo6.style.display = "none";
             diapo5.style.display = "block";
+            diapo4.style.display = "none";
+            diapo3.style.display = "none";
+            diapo2.style.display = "none";
+            diapo1.style.display = "none";
+        }
+        if (diapo === 6) {
+            diapo6.style.display = "block";
+            diapo5.style.display = "none";
             diapo4.style.display = "none";
             diapo3.style.display = "none";
             diapo2.style.display = "none";
